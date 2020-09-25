a0f2ad9c8ff752603cd909b8b56b8770d746e46c
Joseph Ivie <joseph@lightningkite.com>
2020-09-24 18:13:38 -0600

    Removal of actual/shared differentiator in Android

--- butterfly-fcm-android/src/main/java/com/lightningkite/butterfly/fcm/ForegroundNotificationHandler.kt
+++ butterfly-fcm-android/src/main/java/com/lightningkite/butterfly/fcm/ForegroundNotificationHandler.kt
@@ -1,3 +1,4 @@
+//! This file is Khrysalis compatible.
 package com.lightningkite.butterfly.fcm
 
 import com.lightningkite.butterfly.SwiftMustBeClass
--- butterfly-fcm-android/src/main/java/com/lightningkite/butterfly/fcm/Notifications.kt
+++ butterfly-fcm-android/src/main/java/com/lightningkite/butterfly/fcm/Notifications.kt
@@ -13,20 +13,14 @@ object Notifications {
     var notificationToken = StandardObservableProperty<String?>(null)
     @Deprecated("Use 'request' instead", ReplaceWith("this.request"))
     fun configure() = request()
-    fun request(firebaseAppName: String? = null) {
+    fun request() {
         if (Build.VERSION.SDK_INT >= 26) {
             (HttpClient.appContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager).createNotificationChannel(
                 NotificationChannel(HttpClient.appContext.getString(R.string.default_notification_channel_id), "Default", NotificationManager.IMPORTANCE_DEFAULT)
             )
         }
-        if(firebaseAppName != null){
-            FirebaseInstanceId.getInstance(FirebaseApp.getInstance(firebaseAppName)).instanceId.addOnCompleteListener {
-                notificationToken.value = it.result?.token
-            }
-        } else {
-            FirebaseInstanceId.getInstance().instanceId.addOnCompleteListener {
-                notificationToken.value = it.result?.token
-            }
+        FirebaseInstanceId.getInstance().instanceId.addOnCompleteListener {
+            notificationToken.value = it.result?.token
         }
     }
 }
